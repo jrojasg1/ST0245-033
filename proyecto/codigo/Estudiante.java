59ae5b7afeb1f1311011b9461991946a86974a04
jrojasg1 <60229862+jrojasg1@users.noreply.github.com>
2020-05-20 20:36:02 -0500

    Add files via upload

--- proyecto/codigo/Estudiante.java
+++ proyecto/codigo/Estudiante.java
@@ -5,6 +5,8 @@
  */
 package javaapplication17;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author julianrojasgallego
@@ -27,89 +29,94 @@ public class Estudiante {
     private String cole_sede_principal, cole_area_ubicacion, cole_jornada, cole_cod_mcpio_ubicacion, cole_mcpio_ubicacion, cole_cod_depto_ubicacion;
     private String cole_depto_ubicacion, punt_lenguaje, punt_matematicas, punt_biologia, punt_quimica, punt_fisica, punt_ciencias_sociales;
     private String punt_filosofia, punt_ingles, desemp_ingles, profundiza, puntaje_prof, desemp_prof, exito;
-
+    private ArrayList<String> varibles = new ArrayList<String>(70);
     // constructor
     public Estudiante(String estu_consecutivo, String estu_exterior, String periodo, String estu_tieneetnia, String estu_tomo_cursopreparacion, String estu_cursodocentesies, String estu_cursoiesapoyoexterno, String estu_cursoiesexterna, String estu_simulacrotipoicfes, String estu_actividadrefuerzoareas, String estu_actividadrefuerzogeneric, String fami_trabajolaborpadre, String fami_trabajolabormadre, String fami_numlibros, String estu_inst_cod_departamento, String estu_tipodocumento, String estu_nacionalidad, String estu_generoestu, String fechanacimiento, String periodo1, String estu_estudiante, String estu_pais_reside, String estu_depto_reside, String estu_cod_reside_depto, String estu_mcpio_reside, String estu_cod_reside_mcpio, String estu_areareside, String estu_valorpensioncolegio, String fami_educacionpadre, String fami_educacionmadre, String fami_ocupacionpadre, String fami_ocupacionmadre, String fami_estratovivienda, String fami_nivelsisben, String fami_pisoshogar, String fami_tieneinternet, String fami_tienecomputador, String fami_tienemicroondas, String fami_tienehorno, String fami_tieneautomovil, String fami_tienedvd, String fami_tiene_nevera, String fami_tiene_celular, String fami_telefono, String fami_ingresofmiliarmensual, String estu_trabajaactualmente, String estu_antecedentes, String estu_expectativas, String cole_codigo_icfes, String cole_cod_dane_establecimiento, String cole_nombre_establecimiento, String cole_genero, String cole_naturaleza, String cole_calendario, String cole_bilingue, String cole_caracter, String cole_cod_dane_sede, String cole_nombre_sede, String cole_sede_principal, String cole_area_ubicacion, String cole_jornada, String cole_cod_mcpio_ubicacion, String cole_mcpio_ubicacion, String cole_cod_depto_ubicacion, String cole_depto_ubicacion, String punt_lenguaje, String punt_matematicas, String punt_biologia, String punt_quimica, String punt_fisica, String punt_ciencias_sociales, String punt_filosofia, String punt_ingles, String desemp_ingles, String profundiza, String puntaje_prof, String desemp_prof, String exito) {
-        this.estu_consecutivo = estu_consecutivo;
-        this.estu_exterior = estu_exterior;
-        this.periodo = periodo;
-        this.estu_tieneetnia = estu_tieneetnia;
-        this.estu_tomo_cursopreparacion = estu_tomo_cursopreparacion;
-        this.estu_cursodocentesies = estu_cursodocentesies;
-        this.estu_cursoiesapoyoexterno = estu_cursoiesapoyoexterno;
-        this.estu_cursoiesexterna = estu_cursoiesexterna;
-        this.estu_simulacrotipoicfes = estu_simulacrotipoicfes;
-        this.estu_actividadrefuerzoareas = estu_actividadrefuerzoareas;
-        this.estu_actividadrefuerzogeneric = estu_actividadrefuerzogeneric;
-        this.fami_trabajolaborpadre = fami_trabajolaborpadre;
-        this.fami_trabajolabormadre = fami_trabajolabormadre;
-        this.fami_numlibros = fami_numlibros;
-        this.estu_inst_cod_departamento = estu_inst_cod_departamento;
-        this.estu_tipodocumento = estu_tipodocumento;
-        this.estu_nacionalidad = estu_nacionalidad;
-        this.estu_generoestu = estu_generoestu;
-        this.fechanacimiento = fechanacimiento;
-        this.periodo1 = periodo1;
-        this.estu_estudiante = estu_estudiante;
-        this.estu_pais_reside = estu_pais_reside;
-        this.estu_depto_reside = estu_depto_reside;
-        this.estu_cod_reside_depto = estu_cod_reside_depto;
-        this.estu_mcpio_reside = estu_mcpio_reside;
-        this.estu_cod_reside_mcpio = estu_cod_reside_mcpio;
-        this.estu_areareside = estu_areareside;
-        this.estu_valorpensioncolegio = estu_valorpensioncolegio;
-        this.fami_educacionpadre = fami_educacionpadre;
-        this.fami_educacionmadre = fami_educacionmadre;
-        this.fami_ocupacionpadre = fami_ocupacionpadre;
-        this.fami_ocupacionmadre = fami_ocupacionmadre;
-        this.fami_estratovivienda = fami_estratovivienda;
-        this.fami_nivelsisben = fami_nivelsisben;
-        this.fami_pisoshogar = fami_pisoshogar;
-        this.fami_tieneinternet = fami_tieneinternet;
-        this.fami_tienecomputador = fami_tienecomputador;
-        this.fami_tienemicroondas = fami_tienemicroondas;
-        this.fami_tienehorno = fami_tienehorno;
-        this.fami_tieneautomovil = fami_tieneautomovil;
-        this.fami_tienedvd = fami_tienedvd;
-        this.fami_tiene_nevera = fami_tiene_nevera;
-        this.fami_tiene_celular = fami_tiene_celular;
-        this.fami_telefono = fami_telefono;
-        this.fami_ingresofmiliarmensual = fami_ingresofmiliarmensual;
-        this.estu_trabajaactualmente = estu_trabajaactualmente;
-        this.estu_antecedentes = estu_antecedentes;
-        this.estu_expectativas = estu_expectativas;
-        this.cole_codigo_icfes = cole_codigo_icfes;
-        this.cole_cod_dane_establecimiento = cole_cod_dane_establecimiento;
-        this.cole_nombre_establecimiento = cole_nombre_establecimiento;
-        this.cole_genero = cole_genero;
-        this.cole_naturaleza = cole_naturaleza;
-        this.cole_calendario = cole_calendario;
-        this.cole_bilingue = cole_bilingue;
-        this.cole_caracter = cole_caracter;
-        this.cole_cod_dane_sede = cole_cod_dane_sede;
-        this.cole_nombre_sede = cole_nombre_sede;
-        this.cole_sede_principal = cole_sede_principal;
-        this.cole_area_ubicacion = cole_area_ubicacion;
-        this.cole_jornada = cole_jornada;
-        this.cole_cod_mcpio_ubicacion = cole_cod_mcpio_ubicacion;
-        this.cole_mcpio_ubicacion = cole_mcpio_ubicacion;
-        this.cole_cod_depto_ubicacion = cole_cod_depto_ubicacion;
-        this.cole_depto_ubicacion = cole_depto_ubicacion;
-        this.punt_lenguaje = punt_lenguaje;
-        this.punt_matematicas = punt_matematicas;
-        this.punt_biologia = punt_biologia;
-        this.punt_quimica = punt_quimica;
-        this.punt_fisica = punt_fisica;
-        this.punt_ciencias_sociales = punt_ciencias_sociales;
-        this.punt_filosofia = punt_filosofia;
-        this.punt_ingles = punt_ingles;
-        this.desemp_ingles = desemp_ingles;
-        this.profundiza = profundiza;
-        this.puntaje_prof = puntaje_prof;
-        this.desemp_prof = desemp_prof;
-        this.exito = exito;
+        
+        varibles.add(estu_consecutivo);
+        varibles.add(estu_exterior);
+        varibles.add(periodo);
+        varibles.add(estu_tieneetnia);
+        varibles.add(estu_tomo_cursopreparacion);
+        varibles.add(estu_cursodocentesies);
+        varibles.add(estu_cursoiesapoyoexterno);
+        varibles.add(estu_cursoiesexterna);
+        varibles.add(estu_simulacrotipoicfes);
+        varibles.add(estu_actividadrefuerzoareas);
+        varibles.add(estu_actividadrefuerzogeneric);
+        varibles.add(fami_trabajolaborpadre);
+        varibles.add(fami_trabajolabormadre);
+        varibles.add(fami_numlibros);
+        varibles.add(estu_inst_cod_departamento);
+        varibles.add(estu_tipodocumento);
+        varibles.add(estu_nacionalidad);
+        varibles.add(estu_generoestu);
+        varibles.add(fechanacimiento);
+        varibles.add(periodo1);
+        varibles.add(estu_estudiante);
+        varibles.add(estu_pais_reside);
+        varibles.add(estu_depto_reside);
+        varibles.add(estu_cod_reside_depto);
+        varibles.add(estu_mcpio_reside);
+        varibles.add(estu_cod_reside_mcpio);
+        varibles.add(estu_areareside);
+        varibles.add(estu_valorpensioncolegio);
+        varibles.add(fami_educacionpadre);
+        varibles.add(fami_educacionmadre);
+        varibles.add(fami_ocupacionpadre);
+        varibles.add(fami_ocupacionmadre);
+        varibles.add(fami_estratovivienda);
+        varibles.add(fami_nivelsisben);
+        varibles.add(fami_pisoshogar);
+        varibles.add(fami_tieneinternet);
+        varibles.add(fami_tienecomputador);
+        varibles.add(fami_tienemicroondas);
+        varibles.add(fami_tienehorno);
+        varibles.add(fami_tieneautomovil);
+        varibles.add(fami_tienedvd);
+        varibles.add(fami_tiene_nevera);
+        varibles.add(fami_tiene_celular);
+        varibles.add(fami_telefono);
+        varibles.add(fami_ingresofmiliarmensual);
+        varibles.add(estu_trabajaactualmente);
+        varibles.add(estu_antecedentes);
+        varibles.add(estu_expectativas);
+        varibles.add(cole_codigo_icfes);
+        varibles.add(cole_cod_dane_establecimiento);
+        varibles.add(cole_nombre_establecimiento);
+        varibles.add(cole_genero);
+        varibles.add(cole_naturaleza);
+        varibles.add(cole_calendario);
+        varibles.add(cole_bilingue);
+        varibles.add(cole_caracter);
+        varibles.add(cole_cod_dane_sede);
+        varibles.add(cole_nombre_sede);
+        varibles.add(cole_sede_principal);
+        varibles.add(cole_area_ubicacion);
+        varibles.add(cole_jornada);
+        varibles.add(cole_cod_mcpio_ubicacion);
+        varibles.add(cole_mcpio_ubicacion);
+        varibles.add(cole_cod_depto_ubicacion);
+        varibles.add(cole_depto_ubicacion);
+        varibles.add(punt_lenguaje);
+        varibles.add(punt_matematicas);
+        varibles.add(punt_biologia);
+        varibles.add(punt_quimica);
+        varibles.add(punt_fisica);
+        varibles.add(punt_ciencias_sociales);
+        varibles.add(punt_filosofia);
+        varibles.add(punt_ingles);
+        varibles.add(desemp_ingles);
+        varibles.add(profundiza);
+        varibles.add(puntaje_prof);
+        varibles.add(desemp_prof);
+        varibles.add(exito);
+        
+        
     }
-
+    
+    
+    
     public String getEstu_consecutivo() {
         return estu_consecutivo;
     }
@@ -734,4 +741,12 @@ public class Estudiante {
         this.exito = exito;
     }
     
+
+    public ArrayList<String> getVaribles() {
+        return varibles;
+    }
+
+    public void setVaribles(ArrayList<String> varibles) {
+        this.varibles = varibles;
+    }
 }
