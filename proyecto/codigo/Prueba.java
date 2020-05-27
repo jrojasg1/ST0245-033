d7d54c3173080d46d13b020e0d55a0b3bb0f378e
jrojasg1 <60229862+jrojasg1@users.noreply.github.com>
2020-05-26 19:53:00 -0500

    Add files via upload

--- proyecto/codigo/Prueba.java
+++ proyecto/codigo/Prueba.java
@@ -7,6 +7,7 @@ package javaapplication17;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import javafx.util.Pair;
 
 /**
  *
@@ -17,15 +18,15 @@ public class Prueba {
     public static void main(String[] args) throws IOException {
 
         Lectura pru = new Lectura("https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/datasets/0_train_balanced_15000.csv");
-        int cont=0;
+        Arbol a = new Arbol();
         pru.leeDatos();
         LinkedList<Estudiante> ld3 = pru.getEstudiantes();
-        for (Estudiante i : ld3) {
-            
-            System.out.println(i.getExito() );
-            cont++;
-        }
-        System.out.println("contador: "+cont);
+        int cont = 0;
+       
+//        Pair<LinkedList<Estudiante>,LinkedList<Estudiante>>  parejaDeMatrices = a.dividirUnaMatrizEnDosMatrices(ld3, 32, "Estrato 6");      
+//        System.out.println("impureza "+a.laImpurezaDeLosDatosDeUnaMatriz(parejaDeMatrices.getValue()));
+        Pair<Integer,String>  parejaDeMatrices = a.encontrarEnQuePosEstaLaMejorVariableYQueValorDeboCompararEnLaCondicion(ld3);
+        System.out.println(" gini ponderado "+ parejaDeMatrices.getValue() + parejaDeMatrices.getKey()); 
         int dataSize = 1024 * 1024;
         Runtime runtime = Runtime.getRuntime();
         System.out.println("Memoria máxima: " + runtime.maxMemory() / dataSize + "MB");
